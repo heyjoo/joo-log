cf8fb222909c0495c0dc9adffd9c1a559abc7121
Jooeun-K <gangjoo93@gmail.com>
2026-02-06 01:55:34 +0900

    fix link href

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@astrojs/tailwind';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   site: 'https://heyjoo.github.io',
-  base: '/joo-log',
+  base: isProd ? '/joo-log' : '/',
   integrations: [tailwindcss()],
   markdown: {
     shikiConfig: {
