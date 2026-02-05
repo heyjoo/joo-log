d16166caa6884b9817f9085ad48f0817c1bbce5c
Jooeun-K <gangjoo93@gmail.com>
2026-02-06 01:48:47 +0900

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://heyjoo.github.io',
+  base: '/joo-log',
   integrations: [tailwindcss()],
   markdown: {
     shikiConfig: {
